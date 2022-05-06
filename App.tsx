a49bf11bd86226436cecb4793a24604934b4f99f
lee.aki <lee.aki.924@gmail.com>
2022-05-06 13:51:39 +0700

    Update app

--- App.tsx
+++ App.tsx
@@ -1,29 +1,16 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import WalletConnectProvider from '@walletconnect/react-native-dapp';
 import React from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import ConnectWallet from './src/componnets/ConnectWallet/ConnectWallet';
 
 const App: React.FC = () => {
   return (
     <WalletConnectProvider
-      bridge="https://bridge.walletconnect.org"
-      clientMeta={{
-        description: 'Connect with WalletConnect',
-        url: 'https://walletconnect.org',
-        icons: ['https://walletconnect.org/walletconnect-logo.png'],
-        name: 'WalletConnect',
-      }}
-      redirectUrl={Platform.OS === 'web' ? '' : 'yourappscheme://'}
-      storageOptions={{
-        asyncStorage: {
-          store: '',
-          size: '',
-          getStore: '',
-        },
-      }}>
+      storageOptions={{asyncStorage: AsyncStorage}}
+      redirectUrl="myapp://">
       <View style={styles.container}>
-        <Text style={styles.greeting}>ChaiSwap</Text>
+        <Text style={styles.greeting}>ReactNativeDapp</Text>
         <View>
           <ConnectWallet />
         </View>
--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,19 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+const crypto = require.resolve('crypto-browserify');
+const url = require.resolve('url/');
 module.exports = {
   resolver: {
-    extraNodeModules: require('node-libs-react-native'),
+    extraNodeModules: {
+      crypto,
+      url,
+      fs: require.resolve('react-native-fs'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      net: require.resolve('react-native-tcp'),
+      os: require.resolve('os-browserify/browser.js'),
+      path: require.resolve('path-browserify'),
+      stream: require.resolve('readable-stream'),
+      vm: require.resolve('vm-browserify'),
+    },
   },
   transformer: {
     getTransformOptions: async () => ({
